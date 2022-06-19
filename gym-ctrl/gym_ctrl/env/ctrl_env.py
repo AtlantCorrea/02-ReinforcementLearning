cae1dd76c3e83c98021ae79b6df59cad2ba37a71
AtlantCorrea <aocorrea@uc.cl>
2022-06-19 19:13:36 -0400

    Update de CtrlEnv por la versión en el colab 3.6

--- gym-ctrl/gym_ctrl/env/ctrl_env.py
+++ gym-ctrl/gym_ctrl/env/ctrl_env.py
@@ -1,94 +1,167 @@
 import gym
-from gym import spaces
-from gym.utils import seeding
 import numpy as np
-from os import path
+from gym import logger
+from gym import spaces
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import matplotlib.colors as mcolors
+
+def random_value(amplitud):
+  norm_value = (np.random.rand(1)[0]-0.5)*2
+  random_value = norm_value * amplitud
+  return random_value
+
+class CustomEnv(gym.Env):
+    def __init__(self, x0 = random_value(3), ref = random_value(3), T=10):
+    # def __init__(self, x0 = 0, ref = 1, T=10):
+        super(CustomEnv, self).__init__()
+        self.mdel_param = {'m': 1, 'b':1, 'k':1}
+        self.scale_action = 5
+
+        self.T = T
+        self.x0 = x0
+        self.ref = ref
+        self.error = ref-x0
+
+        self.x = np.array([[self.x0,0, self.ref]])
+        self.u = np.array([0])
+        self.t = np.array([0])
+        self.r = np.array([-np.absolute(self.error)])
+        self.dt = 20/1000
+
+        self.x_threshold = 1.5 * np.absolute(self.ref) 
+
+        self.action_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32)
+        # self.observation_space = spaces.Box(low=-10, high=10, shape=(3,), dtype=np.float32)
+
+        # self.steps_beyond_done = None
+        self.epoch = 0
+
+    def reset(self, x0 = random_value(3), ref = random_value(3), T=10):
+    # def reset(self, x0 = 0, ref = 1, T=10):
+        self.T = T
+        self.x0 = x0
+        self.ref = ref
+        self.error = ref-x0
+
+        self.x = np.array([[self.x0,0, self.ref]])
+        self.u = np.array([0])
+        self.t = np.array([0])
+        self.r = np.array([-np.absolute(self.error)])
+        self.dt = 20/1000
+        
+        self.x_threshold = 1.5 * np.absolute(self.ref)
+        observation = self.x[0]
+        self.epoch = 0
+        return np.array(observation, dtype=np.float32)  # reward, done, info can't be included
+
+    def step(self, action):
+        # Timestep
+        t = self.t[-1] + self.dt
+        self.t = np.vstack((self.t, t))
+
+        # Action
+        action = action * self.scale_action
+        self.u = np.vstack((self.u, action))
+
+        # Observation
+        x1, x2 = self.observation(action)
+        observation = np.array([x1,x2,self.ref])
+        self.x = np.vstack((self.x, observation))
+      
+        # Error
+        error = self.get_error(x1)
+        reward = self.reward(error, t)
+        self.r = np.vstack((self.r, reward))
+
+        # Done & Info
+        done, x_limit = self.done(x1, t)
+        if x_limit:
+          reward = -1e9
+        info = self.info(error, reward)
+
+        self.epoch+=1
+        return np.array(observation, dtype=np.float32), reward, done, info
+
+    def render(self):
+        print('render estaba implementado pero lo eliminé, buscar en versiones 3.0')
+
+    def close (self):
+        pass
+
+    def observation(self, action):
+        u = action
+        x = self.x[-1]
+        t_i = np.linspace(0, self.dt, 5)
+        sol_array =  odeint(self.model, x[:-1], t_i, args = (u,))
+        return sol_array[-1]
+
+    def reward(self, error, t):
+        reward = -np.absolute(error) * (1 + float(t/2)**1.2) 
+        return reward
+
+    def done(self, x1, t):
+        x_limit = bool( np.absolute(x1) > self.x_threshold)
+        t_limit = bool( t > self.T)
+        done = x_limit or t_limit
+        return done, x_limit
+
+    def info(self, error, reward):
+        info = {'x':self.x,
+                'u':self.u,
+                't':self.t,
+                'r':self.r,
+                'dt':self.dt,
+                'ref':self.ref,
+                'error': error,
+                'epoch': self.epoch,
+                # 'episode':{'r':reward,'l':self.epoch}}
+                'episode':{'r':3,'l':4}}
+        return info
+
+    def get_error(self, obs):
+        return self.ref - obs
+        
+    def model(self, x, t, u = 0):
+        x1,x2 = x
+        m = self.mdel_param['m']    
+        k = self.mdel_param['k']    
+        b = self.mdel_param['b']
+        
+        dx1dt = x2
+        dx2dt = -k/m*x1 -b/m*x2 + u/m 
+        return [dx1dt, dx2dt]
 
 
-class CtrlEnv(gym.Env):
-    metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 30}
-
-    def __init__(self, g=10.0):
-        self.max_speed = 8
-        self.max_torque = 2.0
-        self.dt = 0.05
-        self.g = g
-        self.m = 1.0
-        self.l = 1.0
-        self.viewer = None
-
-        high = np.array([1.0, 1.0, self.max_speed], dtype=np.float32)
-        self.action_space = spaces.Box(
-            low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32
-        )
-        self.observation_space = spaces.Box(low=-high, high=high, dtype=np.float32)
-
-        self.seed()
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
-
-    def step(self, u):
-        th, thdot = self.state  # th := theta
-
-        g = self.g
-        m = self.m
-        l = self.l
-        dt = self.dt
-
-        u = np.clip(u, -self.max_torque, self.max_torque)[0]
-        self.last_u = u  # for rendering
-        costs = angle_normalize(th) ** 2 + 0.1 * thdot ** 2 + 0.001 * (u ** 2)
-
-        newthdot = thdot + (3 * g / (2 * l) * np.sin(th) + 3.0 / (m * l ** 2) * u) * dt
-        newthdot = np.clip(newthdot, -self.max_speed, self.max_speed)
-        newth = th + newthdot * dt
-
-        self.state = np.array([newth, newthdot])
-        return self._get_obs(), -costs, False, {}
-
-    def reset(self):
-        high = np.array([np.pi, 1])
-        self.state = self.np_random.uniform(low=-high, high=high)
-        self.last_u = None
-        return self._get_obs()
-
-    def _get_obs(self):
-        theta, thetadot = self.state
-        return np.array([np.cos(theta), np.sin(theta), thetadot], dtype=np.float32)
-
-    def render(self, mode="human"):
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-
-            self.viewer = rendering.Viewer(500, 500)
-            self.viewer.set_bounds(-2.2, 2.2, -2.2, 2.2)
-            rod = rendering.make_capsule(1, 0.2)
-            rod.set_color(0.8, 0.3, 0.3)
-            self.pole_transform = rendering.Transform()
-            rod.add_attr(self.pole_transform)
-            self.viewer.add_geom(rod)
-            axle = rendering.make_circle(0.05)
-            axle.set_color(0, 0, 0)
-            self.viewer.add_geom(axle)
-            fname = path.join(path.dirname(__file__), "assets/clockwise.png")
-            self.img = rendering.Image(fname, 1.0, 1.0)
-            self.imgtrans = rendering.Transform()
-            self.img.add_attr(self.imgtrans)
-
-        self.viewer.add_onetime(self.img)
-        self.pole_transform.set_rotation(self.state[0] + np.pi / 2)
-        if self.last_u is not None:
-            self.imgtrans.scale = (-self.last_u / 2, np.abs(self.last_u) / 2)
-
-        return self.viewer.render(return_rgb_array=mode == "rgb_array")
-
-    def close(self):
-        if self.viewer:
-            self.viewer.close()
-            self.viewer = None
-
-
-def angle_normalize(x):
-    return ((x + np.pi) % (2 * np.pi)) - np.pi
+    def plot_info(self, ax, info, title='', xlabel='', x=True, x_dot=True, u=True, r=True, error=True, ref=True):
+        time = info['t']
+        if x:
+          color = mcolors.TABLEAU_COLORS['tab:olive']
+          line = '-'
+          ax.plot(time, info['x'][:,0], color = color, linestyle= line, label='Obs. x')
+        if x_dot:
+          color = mcolors.TABLEAU_COLORS['tab:gray']
+          line= '-'
+          ax.plot(time, info['x'][:,1], color = color, linestyle= line, label="Obs. x'")
+        if u:
+          color = mcolors.TABLEAU_COLORS['tab:green']  
+          line= '-'
+          ax.plot(time, info['u'], color = color, linestyle= line, label='Action u')
+        if r:
+          color = mcolors.TABLEAU_COLORS['tab:red']
+          line = '-'
+          ax.plot(time, info['r'], color = color, linestyle= line, label='Reward')
+        if error:
+          color = mcolors.TABLEAU_COLORS['tab:brown']
+          line= '-'
+          ax.plot(time, info['x'][:,2] - info['x'][:,0], color = color, linestyle= line, label='Error')
+        if ref:
+          ax.plot(time, np.ones(len(time))*info['ref'] , 'k:', label='Reference')
+        ax.plot(time, np.zeros(len(time)) , 'k-', linewidth=0.7)
+        
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.legend(bbox_to_anchor=(1.04,1), borderaxespad=0)
+        ax.grid()
+        return ax
