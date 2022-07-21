3e7ebd21474d82678d40845e09b7da734d9eb3b8
AtlantCorrea <aocorrea@uc.cl>
2022-07-21 00:29:27 -0400

    Agente con acciones aleatorias

--- ctrl-train/ctrl_run.py
+++ ctrl-train/ctrl_run.py
@@ -2,8 +2,7 @@ import gym
 from ctrl_env.ctrl_env import CtrlEnv
 
 env = CtrlEnv()
-env.reset()
-observation = env.reset()
+observation = env.reset(ref=1)
 for _ in range(1000):
    action = env.action_space.sample()  
    observation, reward, done, info = env.step(action)
@@ -11,5 +10,4 @@ for _ in range(1000):
    if done:
       break
 env.render()
-env.reset()
 input('ok?')
